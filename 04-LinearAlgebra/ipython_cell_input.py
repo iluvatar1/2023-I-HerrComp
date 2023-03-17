f5778150872b43d6d78b095b85ce4ef00da00868
iluvatar1 <woquendo@gmail.com>
2023-03-17 10:25:32 -0500

    Added latest class changes

--- 04-LinearAlgebra/ipython_cell_input.py
+++ 04-LinearAlgebra/ipython_cell_input.py
@@ -2,23 +2,28 @@ from scipy import linalg
 import numpy as np
 import time # use time.process_time_ns() , compare with monotonic_ns()
 
+# WARNINGS
+# 1) Seed is not controlled. Expect different results per run
+
 MINSIZE=1
-MAXSIZE=5000
+MAXSIZE=7000
 NSAMPLES=10
 data = np.zeros((NSAMPLES, 3))
-### BEGIN SOLUTION
+# YOUR CODE HERE
+N = np.geomspace(MINSIZE, MAXSIZE, NSAMPLES, dtype=np.int64)
+#print(N)
 
-for ii,N in enumerate(np.geomspace(MINSIZE, MAXSIZE, num=NSAMPLES)) :
-    A = np.random.rand(int(N), int(N))
-    start = time.process_time_ns() 
-    #start = time.monotonic_ns() 
+data = np.zeros((NSAMPLES, 2))
+
+for ii in range(len(N)):
+    A = np.random.rand(N[ii], N[ii])
+    start = time.process_time_ns()
+    #start = time.monotonic_ns()
     kappa = linalg.norm(A)*linalg.norm(linalg.inv(A))
-    end = time.process_time_ns() 
-    #end = time.monotonic_ns() 
-    elapsed = (end-start)*1.0e-9
-    data[ii, 0] = int(N)
-    data[ii, 1] = kappa
-    data[ii, 2] = elapsed
-    print(f"{int(N)}  {kappa}  {elapsed}")
-### END SOLUTION
-    
+    end = time.process_time_ns()
+    #end = time.monotonic_ns()
+    #print(f"{kappa=}")
+    data[ii, 0] = N[ii]
+    total_time = (end - start)*1.0e-9
+    data[ii, 1] = total_time
+    print(f"{N[ii]}\t {total_time}")
